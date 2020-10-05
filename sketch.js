4abe1ff5cbe10e77e5a377188d0ce172d16d0371
jothir278 <68913031+jothir278@users.noreply.github.com>
2020-10-05 22:20:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var database,canvas;
+var form,game,player;
+var gameState = 0;
+var playerCount;
+
+
+function setup(){
+    database = firebase.database();
+    canvas = createCanvas(500,500);
+    
+    game = new Game();
+    game.getState();
+    game.start();
+
+
+}
+
+function draw(){
+    background("white");
+
+    
+}
+
